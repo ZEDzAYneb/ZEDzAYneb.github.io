17dfc5bcc8cb3a0a01a0dd7dae8cbcc877b944b7
ZEDzAYneb <zaynebbenzo@gmail.com>
2021-05-14 09:16:21 +0200

    Initial commit

---
+++ static/skin_classes.js
@@ -0,0 +1,9 @@
+SKIN_CLASSES = {
+    0: 'Actinic Keratosis',
+    1: 'Basal Cell Carcinoma',
+    2: 'Benign Keratosis',
+    3: 'Dermatofibroma',
+    4: 'Melanoma',
+    5: 'Melanocytic Nevi',
+    6: 'Vascular Lesion'
+  };
