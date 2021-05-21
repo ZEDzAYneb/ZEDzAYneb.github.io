c6899ee4aa4fd80d8004c8fd34e831fe978bfede
ZEDzAYneb <zaynebbenzo@gmail.com>
2021-05-21 10:44:04 +0200

    Delete static directory

--- static/skin_classes.js
+++
@@ -1,9 +0,0 @@
-SKIN_CLASSES = {
-    0: 'Actinic Keratosis',
-    1: 'Basal Cell Carcinoma',
-    2: 'Benign Keratosis',
-    3: 'Dermatofibroma',
-    4: 'Melanoma',
-    5: 'Melanocytic Nevi',
-    6: 'Vascular Lesion'
-  };
